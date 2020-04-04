314292fc6e43599058765967fb0204d726a5e025
leonardo vizzotto <vizzotto_leonardo@terceiros.sicredi.com.br>
2020-04-04 19:23:43 -0300

    change in colors

---
+++ src/components/Post.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { Link } from "gatsby"
+import { rhythm } from "../utils/typography"
+
+const GitCorner = ({ node }) => {
+  const title = node.frontmatter.title || node.fields.slug
+  return (
+    <article >
+      <header>
+        <h3
+          style={{
+            marginBottom: rhythm(1 / 4),
+          }}
+        >
+          <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+            {title}
+          </Link>
+        </h3>
+        <small>{node.frontmatter.date}</small>
+      </header>
+      <section>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: node.frontmatter.description || node.excerpt,
+          }}
+        />
+      </section>
+    </article>
+  )
+}
+export default GitCorner
