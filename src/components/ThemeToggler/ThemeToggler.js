62f334e6bc8d716f86b221731205f902020cbb61
leonardo vizzotto <vizzotto_leonardo@terceiros.sicredi.com.br>
2020-06-06 22:21:45 -0300

    fix-build

--- src/components/ThemeToggler/ThemeToggler.js
+++ src/components/ThemeToggler/ThemeToggler.js
@@ -1,23 +1,28 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import styles from "./themeToggler.module.scss"
 
 const ThemeToggler = () => {
-  const [value] = useState(localStorage.getItem("theme") === "dark");
+  const [value, setValue] = useState(true);
 
-  const onChange = ({ target }) => {
+  useEffect(() => {
+    setValue(localStorage.getItem("theme") === "dark")
+  }, [])
+
+  const onClick = ({ target }) => {
     if (target.checked) {
       document.body.classList.replace("light", "dark")
     } else {
       document.body.classList.replace("dark", "light")
     }
     localStorage.setItem("theme", target.checked ? "dark" : "light")
+    setValue(target.checked);
   }
 
   return (
     <div className={styles.toggler}>
       <input
-        defaultChecked={value}
-        onChange={onChange}
+        checked={value}
+        onClick={onClick}
         type="checkbox"
         className={styles.checkbox}
       />
