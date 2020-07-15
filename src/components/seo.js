cac7c404d25eb18d535f0fa922a7bacffcacaf08
leonardo vizzotto <vizzotto_leonardo@terceiros.sicredi.com.br>
2020-07-15 11:49:33 -0300

    update twitter image origin

--- src/components/seo.js
+++ src/components/seo.js
@@ -37,7 +37,7 @@ const SEO = ({ description, lang, meta, title }) => {
 
   const metaDescription = description || site.siteMetadata.description
 
-  let origin = ""
+  let origin = "https://lenes.com.br"
   if (typeof window !== "undefined") {
     origin = window.location.origin
   }
